ba97acf4789d5c93fc770d01c9805c1a01ec3fe3
jackbasso <jackbasso.r@gmail.com>
2022-10-18 16:10:42 -0500

    product model

--- petnet/store/models.py
+++ petnet/store/models.py
@@ -11,3 +11,19 @@ class Category(models.Model):
 
     def __str__(self):
         return self.title
+
+class Product(models.Model):
+    user = models.ForeignKey(User, related_name="products", on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, related_name="products", on_delete=models.CASCADE)
+    title = models.CharField(max_length=50)
+    slug = models.SlugField(max_length=50)
+    description = models.TextField(blank=True)
+    price = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+    
+    def get_display_price(self):
+        return self.price / 100
