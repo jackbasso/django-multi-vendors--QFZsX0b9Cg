36c86d30c75860eccbba5a9d16b5c9a37335be8e
jackbasso <jackbasso.r@gmail.com>
2022-10-18 17:26:20 -0500

    product detail view

--- petnet/store/models.py
+++ petnet/store/models.py
@@ -22,6 +22,9 @@ class Product(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+    class Meta:
+        ordering = ('-created_at',)
+
     def __str__(self):
         return self.title
     
