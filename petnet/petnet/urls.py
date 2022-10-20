2c74ab1efabd23e0164a78ad93c68ff18f5e7cb0
jackbasso <jackbasso.r@gmail.com>
2022-10-20 16:53:34 -0500

    add image

--- petnet/petnet/urls.py
+++ petnet/petnet/urls.py
@@ -1,11 +1,13 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from core.views import frontpage, about
 
 urlpatterns = [
-    path('', frontpage, name="frontpage"),
-    path('', include('store.urls')),
     path('about/', about, name="about"),
     path('admin/', admin.site.urls),
-
-]
+    path('', frontpage, name="frontpage"),
+    path('', include('store.urls')),
+    
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
