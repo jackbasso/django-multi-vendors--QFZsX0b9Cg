d2b24f51be68afa4ebebed30718832aa91760da2
jackbasso <jackbasso.r@gmail.com>
2022-10-20 18:04:19 -0500

    signup

--- petnet/userprofile/urls.py
+++ petnet/userprofile/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+  path('signup/', views.signup, name='signup'),
   path('vendors/<int:pk>/', views.vendor_detail, name='vendor_detail'),
 ]
--- petnet/userprofile/views.py
+++ petnet/userprofile/views.py
@@ -1,6 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
+from .models import Userprofile
 
 def vendor_detail(request, pk):
   user = User.objects.get(pk=pk)
   return render(request, 'userprofile/vendor_detail.html', { 'user':user})
+
+def signup(request):
+  if request.method == 'POST':
+    form = UserCreationForm(request.POST)
+
+    if form.is_valid():
+      user = form.save()
+
+      login(request, user)
+
+      userprofile = Userprofile.objects.create(user=user)
+      return redirect('frontpage')
+  else:
+    form = UserCreationForm()
+  
+  return render(request, 'userprofile/signup.html', {'form':form})
